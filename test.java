cb3e9d24a7a22199932dc5a5dd8f1a63482c08a8
Babe <tonnum.kawin@gmail.com>
2023-11-27 11:45:51 +0700

    test

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+
+    public static void main(String[] args) {
+        String s = new String("Hello World");
+        System.out.println(s);
+        int weight = 50, age = 12 ;
+        System.out.println(weight + age);
+        final double KIKEW = 32.89 ;
+        String lol = new String("MyKDA") ;
+        System.out.println(lol + " " + KIKEW);
+    }
+}
