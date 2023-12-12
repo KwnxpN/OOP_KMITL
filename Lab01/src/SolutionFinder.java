9642cdaa1198dc5c7a557c2516c29d8f6e8a0259
Admin <tonnum.kawin@gmail.com>
2023-12-12 20:21:01 +0700

    Test github with NetBeans

---
+++ Lab01/src/SolutionFinder.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+public class SolutionFinder {
+    public static void main(String[] args) {
+        double a = 4, b = 8, c = 3;
+        double x1 = ((0 - b) + (Math.sqrt(Math.pow(b, 2) - (4 * a * c)))) / (2 * a);
+        double x2 = ((0 - b) - (Math.sqrt(Math.pow(b, 2) - (4 * a * c)))) / (2 * a);
+        System.out.println(x1);
+        System.out.println(x2);
+    }
+}
