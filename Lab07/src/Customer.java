1d222bfd7e96801d378d7ff40e4c0b15df33cd74
Kawin Isaramala <138878357+KwnxpN@users.noreply.github.com>
2024-02-01 21:09:37 +0700

    Add files via upload

---
+++ Lab07/src/Customer.java
@@ -0,0 +1,51 @@
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private CheckingAccount acct;
+    public Customer() {
+        this("", "", null);
+    }
+    public Customer(String firstName, String lastName) {
+        this(firstName, lastName, null);
+    }
+    public Customer(String firstName, String lastName, CheckingAccount acct) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.acct = acct;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setAcct(CheckingAccount acct) {
+        this.acct = acct;
+    }
+    public CheckingAccount getAcct() {
+        return acct;
+    }
+    @Override
+    public String toString() {
+        if (getAcct() == null) {
+            return getFirstName() + " "+ getLastName() + " doesn't have account.";
+        }
+        else {
+            return "The "+ getFirstName() + " account has "+ getAcct().getBalance() + " baht and " + getAcct().getCredit() + " credits.";
+        }
+    }
+    public boolean equals(Customer c) {
+        if ((c.getFirstName().equals(this.getFirstName())) && (c.getLastName().equals(this.getLastName()))) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
