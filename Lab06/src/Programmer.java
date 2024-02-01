120186d82f4f1fe02822fc8ad6969217c17a491f
Kawin Isaramala <138878357+KwnxpN@users.noreply.github.com>
2024-02-01 15:43:24 +0700

    Add files via upload

---
+++ Lab06/src/Programmer.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Programmer extends Employee {
+    private int happiness;
+    public void coding(String str) {
+        if (getEnergy() >= 30) {
+            System.out.println("Your code is "+str);
+            setEnergy(getEnergy() - 30);
+            setHappiness(getHappiness() - 30);
+        }
+        else {
+            System.out.println("Error Error Error");
+            setEnergy(getEnergy() - 30);
+            setHappiness(getHappiness() - 30);
+        }
+    }
+    public void coding(char str) {
+        this.coding(String.valueOf(str));
+    }
+    public int getHappiness() {
+        return happiness;
+    }
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
+    }
+}
