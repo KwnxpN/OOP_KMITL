822799bccf272d663e4af969fba2a6ab0c609dd6
Admin <tonnum.kawin@gmail.com>
2024-02-09 10:28:26 +0700

    Fixed

--- Lab08/src/MainMDI.java
+++ Lab08/src/MainMDI.java
@@ -1,14 +1,6 @@
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author Admin
- */
 public class MainMDI {
 
     public static void main(String[] args) {
