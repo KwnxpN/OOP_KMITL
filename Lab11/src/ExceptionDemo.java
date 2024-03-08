ad23aa0d18fa724149bee4fccf43c249560592e1
Admin <tonnum.kawin@gmail.com>
2024-03-08 09:50:28 +0700

    javac lab11

--- Lab11/src/ExceptionDemo.java
+++ Lab11/src/ExceptionDemo.java
@@ -9,9 +9,15 @@ public class ExceptionDemo {
             double discriminant = Math.sqrt(b * b - 4 * a * c);
             double x1 = (-b + discriminant) / (2 * a);
             double x2 = (-b - discriminant) / (2 * a);
-
-            System.out.println("x1 = " + x1);
-            System.out.println("x2 = " + x2);
+            
+            if (args.length > 3) {
+                 System.out.println("Please enter 3 numbers as a, b, and c respectively.");
+            }
+            else {
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
+        
         }
         catch (NumberFormatException e) {
             System.out.println("Please input data in number format only.");
